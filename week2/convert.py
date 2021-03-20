08805932b2a9290251969f43a24e1391119329ab
Trần Tiến Đạt <67142437+18520591@users.noreply.github.com>
2021-03-20 21:47:21 +0700

    Update convert.py

--- week2/convert.py
+++ week2/convert.py
@@ -23,21 +23,35 @@ def rgb_to_hsv(r, g, b):
         s = (diff / mx) * 100
     v = mx * 100
     return h, s, v
+
 img = cv.imread('/home/dat/MicrochipDesign/LAB1/anh.png')
 (height, width,depth) = img.shape
+h_v = [[0 for x in range(width)] for y in range(height)]
+s_v = [[0 for x in range(width)] for y in range(height)]
+v_v = [[0 for x in range(width)] for y in range(height)]
+f = open("/home/dat/MicrochipDesign/LAB1/hsv.txt","r")
+for i in range(0, height):
+    for j in range(0, width):
+        a = f.readline()
+        h_v[i][j] = round(int(a)*255/360)
+        a = f.readline()  
+        s_v[i][j] = round(int(a)*2.55)
+        a = f.readline()
+        v_v[i][j] = round(int(a)*2.55) 
+f.close()
+h_v = np.array(h_v)
+s_v = np.array(s_v)
+v_v = np.array(v_v)
+hsv_v = cv.merge([h_v, s_v, v_v])
+
 h = [[0 for x in range(width)] for y in range(height)]
 s = [[0 for x in range(width)] for y in range(height)]
 v = [[0 for x in range(width)] for y in range(height)]
 
-f = open("/home/dat/MicrochipDesign/LAB1/rgb.txt","w")
 for i in range(0,height):
     for j in range(0,width):
         (B, G, R) = img[i][j]
         h_t, s_t, v_t = rgb_to_hsv(R, G, B)
-        b = str(B)
-        g = str(G)
-        r = str(R)
-        f.write(r + " " + g + " " + b +"\n")
         h_t = round(255*h_t/360)
         s_t = round(s_t*2.55)
         v_t = round(v_t*2.55)
@@ -45,14 +59,14 @@ for i in range(0,height):
         s[i][j] = s_t
         v[i][j] = v_t
 
-f.close()
 h = np.array(h)
 s = np.array(s)
 v = np.array(v)
-hsv = cv.merge([v, s, h])
-plt.imshow(hsv)
-plt.title('anh HSV xu ly theo python', fontweight ="bold")
-plt.show()
+hsv = cv.merge([h, s, v])
 
+hsv_v = hsv_v.astype(np.uint8)
+cv.imshow("hsv verilog", hsv_v)
+hsv = hsv.astype(np.uint8)
+cv.imshow("hsv python", hsv)
 
-
+cv.waitKey(0)
