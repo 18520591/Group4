a7fd2c8a3c8fa1fd6eb0962800474ec75c5b0548
Vũ Đức Thế <52497949+kevinvu2307@users.noreply.github.com>
2021-03-20 21:57:54 +0700

    Update main.py

--- week2/main.py
+++ week2/main.py
@@ -1,1 +1,5 @@
-
+import os
+os.system('python3 rgb2txt.py')
+os.system('vlog "/home/thevu/Desktop/CE434/week2/LAB1/convert/rgb_to_hsv.v" "/home/thevu/Desktop/CE434/week2/LAB1/convert/testbench.v"')
+os.system('vsim -c -do "run -all" testbench.v')
+os.system('python3 "/home/thevu/Desktop/CE434/week2/LAB1/convert.py"')
