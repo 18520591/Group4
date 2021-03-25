58eaf70c77c66a09f2345d1b310a7b36caa23eec
Trần Tiến Đạt <67142437+18520591@users.noreply.github.com>
2021-03-25 15:34:53 +0700

    Add files via upload

---
+++ week3/convert_verilog.py
@@ -0,0 +1,101 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+#convert from binary to decimal
+def bin_to_dec(binary):
+    decimal = 0
+    for digit in binary:
+        decimal = decimal*2 + int(digit)
+    return decimal
+
+#convert from Floating point to float(+)
+def FloatingPoint_to_float(fp):
+    if (fp[1:len(fp)] == '0000000000000000000000000000000'):
+        return 0
+    ex = fp[1:9]
+    fr = fp[9:len(fp)]
+    ex = bin_to_dec(ex) - 127
+    t = 0
+    d = 0
+    for i in fr:
+        d = d + 1
+        if i == '1':
+            t = t + pow(2, -d)
+    fr = t + 1
+    return fr * pow(2,ex)
+
+
+def rgb_to_hsv(r, g, b):
+    r = r / 255.0
+    g = g / 255.0
+    b = b / 255.0
+    mx = max(r, g, b)
+    mn = min(r, g, b)
+    diff = mx - mn
+    if (mx == mn):
+        h = 0
+    elif mx == r:
+        h = abs(g-b)*43/diff
+    elif mx == g:
+        h = abs(g-b)*43/diff + 85
+    elif mx == b:
+        h = abs(g-b)*43/diff + 171
+    if mx == 0:
+        s = 0
+    else:
+        s = (diff / mx)
+    v = mx
+    return h, s, v
+
+img = cv.imread('/home/dat/MicrochipDesign/LAB1/anh1.jpeg')
+(height, width,depth) = img.shape
+h_v = [[0 for x in range(width)] for y in range(height)]
+s_v = [[0 for x in range(width)] for y in range(height)]
+v_v = [[0 for x in range(width)] for y in range(height)]
+f = open("/home/dat/MicrochipDesign/LAB1/hsv.txt","r")
+for i in range(0, height):
+    for j in range(0, width):
+        a = f.readline()
+        h_t = round(FloatingPoint_to_float(a)/360*255)
+        h_v[i][j] = h_t
+        a = f.readline()  
+        s_t = round(FloatingPoint_to_float(a)*255)
+        s_v[i][j] = s_t
+        a = f.readline()
+        v_t = round(FloatingPoint_to_float(a)*255)
+        v_v[i][j] = v_t 
+f.close()
+h_v = np.array(h_v)
+s_v = np.array(s_v)
+v_v = np.array(v_v)
+#hsv_v = cv.merge([h_v, s_v, v_v])
+
+h = [[0 for x in range(width)] for y in range(height)]
+s = [[0 for x in range(width)] for y in range(height)]
+v = [[0 for x in range(width)] for y in range(height)]
+
+for i in range(0,height):
+    for j in range(0,width):
+        (B, G, R) = img[i][j]
+        h_t, s_t, v_t = rgb_to_hsv(R, G, B)
+        h_t = round(255*h_t/360)
+        s_t = round(s_t*255)
+        v_t = round(v_t*255)
+        h[i][j] = h_t
+        s[i][j] = s_t
+        v[i][j] = v_t
+
+h = np.array(h)
+s = np.array(s)
+v = np.array(v)
+hsv = cv.merge([h, s, v])
+'''
+hsv_v = hsv_v.astype(np.uint8)
+cv.imshow("hsv verilog", hsv_v)
+hsv = hsv.astype(np.uint8)
+cv.imshow("hsv python", hsv)
+'''
+print(type(h))
+print(type(h_v))
+cv.waitKey(0)
