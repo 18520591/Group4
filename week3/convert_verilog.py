fe005a3a9655e623682c9e135e41b239dec0fba7
Vũ Đức Thế <52497949+kevinvu2307@users.noreply.github.com>
2021-03-26 21:20:15 +0700

    Update convert_verilog.py

--- week3/convert_verilog.py
+++ week3/convert_verilog.py
@@ -48,12 +48,12 @@ def rgb_to_hsv(r, g, b):
     v = mx
     return h, s, v
 
-img = cv.imread('/home/dat/MicrochipDesign/LAB1/anh1.jpeg')
+img = cv.imread('/home/thevu/Desktop/CE434/week2/LAB1/lucbinh.jpg')
 (height, width,depth) = img.shape
 h_v = [[0 for x in range(width)] for y in range(height)]
 s_v = [[0 for x in range(width)] for y in range(height)]
 v_v = [[0 for x in range(width)] for y in range(height)]
-f = open("/home/dat/MicrochipDesign/LAB1/hsv.txt","r")
+f = open("/home/thevu/Desktop/CE434/week2/LAB1/hsv.txt","r")
 for i in range(0, height):
     for j in range(0, width):
         a = f.readline()
@@ -79,9 +79,9 @@ for i in range(0,height):
     for j in range(0,width):
         (B, G, R) = img[i][j]
         h_t, s_t, v_t = rgb_to_hsv(R, G, B)
-        h_t = round(255*h_t/360)
-        s_t = round(s_t*255)
-        v_t = round(v_t*255)
+        h_t = (round(255*h_t/360))
+        s_t = (round(s_t*255))
+        v_t = (round(v_t*255))
         h[i][j] = h_t
         s[i][j] = s_t
         v[i][j] = v_t
@@ -89,13 +89,15 @@ for i in range(0,height):
 h = np.array(h)
 s = np.array(s)
 v = np.array(v)
-hsv = cv.merge([h, s, v])
-'''
-hsv_v = hsv_v.astype(np.uint8)
-cv.imshow("hsv verilog", hsv_v)
-hsv = hsv.astype(np.uint8)
-cv.imshow("hsv python", hsv)
-'''
-print(type(h))
-print(type(h_v))
-cv.waitKey(0)
+hsv_f = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+h_f, s_f, v_f = cv.split(hsv_f)
+e_py = 0
+e_v = 0
+for i in range(0, height):
+    for j in range(0, width):
+        e_py = abs(h[i][j] - h_f[i][j]) + abs(s[i][j] - s_f[i][j]) + abs(v[i][j] - v_f[i][j]) + e_py
+        e_v = abs(h_v[i][j] - h_f[i][j]) + abs(s_v[i][j] - s_f[i][j]) + abs(v_v[i][j] - v_f[i][j]) + e_v
+e_v = float(e_v / (height*width*depth*255))
+e_py = float(e_py / (height*width*depth*255))
+print("verilog",e_v)
+print("python",e_py)
