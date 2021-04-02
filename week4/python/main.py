1b49eeef9c7bc46886247692727fe1842b6347a3
Trần Tiến Đạt <67142437+18520591@users.noreply.github.com>
2021-04-02 21:57:30 +0700

    Add files via upload

---
+++ week4/python/main.py
@@ -0,0 +1,9 @@
+import os
+import cv2
+import numpy as np
+
+os.system('python3 /home/dat/MicrochipDesign/LAB1/rgb2txt.py')
+
+os.system('vlog "/home/dat/MicrochipDesign/LAB1/convert_fl/Convert.v" "/home/dat/MicrochipDesign/LAB1/convert_fl/testbench.v"')
+os.system('vsim -c -do "run -all" testbench')
+os.system('python3 /home/dat/MicrochipDesign/LAB1/convert_verilog.py')
